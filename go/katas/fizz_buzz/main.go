39ad68967201b5ebac72dff76712c193a75732d3
SGuzmanBeltran <santiagoguzmanbeltran@gmail.com>
2025-04-02 16:58:23 -0500

    progress

--- go/katas/fizz_buzz/main.go
+++ go/katas/fizz_buzz/main.go
@@ -4,7 +4,8 @@ import (
 	"strconv"
 )
 
-type FizzBuzz struct {}
+// TODO: We're currently starting the FOURTH CYCLE of https://www.codurance.com/katas/fizzbuzz
+type FizzBuzz struct{}
 
 func (fb *FizzBuzz) Convert(number int) (string, error) {
 	return strconv.Itoa(number), nil
