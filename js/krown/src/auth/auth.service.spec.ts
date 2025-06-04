f75f6e37c88e5618b0d8944f827c3a36599789bf
SGuzmanBeltran <santiagoguzmanbeltran@gmail.com>
2025-06-03 21:15:08 -0500

    my first tests

--- js/krown/src/auth/auth.service.spec.ts
+++ js/krown/src/auth/auth.service.spec.ts
@@ -1,3 +1,5 @@
+import * as bcrypt from 'bcryptjs';
+
 import { BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
@@ -5,10 +7,15 @@ import { AuthService } from './auth.service';
 import { DRIZZLE } from '../drizzle/drizzle.module';
 import { DrizzleDB } from '../drizzle/types/drizzle';
 import { JwtService } from '@nestjs/jwt';
+import { users } from '@src/drizzle/schemas/users.schema';
 
 jest.mock('bcryptjs', () => ({
-  hash: jest.fn().mockResolvedValue('hashedPassword'),
-  compare: jest.fn().mockResolvedValue(true),
+  hash: jest.fn().mockImplementation((password) => {
+    return Promise.resolve(`hashed${password}`);
+  }),
+  compare: jest.fn().mockImplementation((password, hashedPassword) => {
+    return Promise.resolve(`hashed${password}` === hashedPassword);
+  }),
 }));
 
 describe('AuthService', () => {
@@ -67,6 +74,68 @@ describe('AuthService', () => {
       );
     });
 
+    it.each([
+      {
+        scenario: 'existing email',
+        registerTestDto: {
+          email: 'test@example.com',
+          username: 'testuser',
+        },
+        existingUser: {
+          id: 1,
+          email: 'test@example.com',
+          username: 'different',
+        },
+      },
+      {
+        scenario: 'existing username',
+        registerTestDto: {
+          email: 'test@example.com',
+          username: 'testuser',
+        },
+        existingUser: {
+          id: 2,
+          email: 'different@example.com',
+          username: 'testuser',
+        },
+      },
+      {
+        scenario: 'existing email and username',
+        registerTestDto: {
+          email: 'test@example.com',
+          username: 'testuser',
+        },
+        existingUser: {
+          id: 3,
+          email: 'test@example.com',
+          username: 'testuser',
+        },
+      },
+    ])(
+      'should throw BadRequestException when $scenario',
+      async ({ registerTestDto, existingUser }) => {
+        const registerDto = {
+          email: registerTestDto.email,
+          password: 'password',
+          passwordConfirmation: 'password',
+          username: registerTestDto.username,
+          cellphone: '1234567890',
+        };
+
+        (mockDrizzle.select as jest.Mock).mockReturnValueOnce({
+          from: jest.fn().mockReturnValue({
+            where: jest.fn().mockReturnValue({
+              limit: jest.fn().mockResolvedValue([existingUser]),
+            }),
+          }),
+        });
+
+        await expect(service.register(registerDto)).rejects.toThrow(
+          BadRequestException,
+        );
+      },
+    );
+
     it('should register a new user successfully', async () => {
       (mockDrizzle.select as jest.Mock).mockReturnValueOnce({
         from: jest.fn().mockReturnValue({
@@ -87,27 +156,65 @@ describe('AuthService', () => {
       const result = await service.register(registerDto);
       expect(result).toEqual({ message: 'User registered successfully' });
     });
+
+    it('should hash password', async () => {
+      const registerDto = {
+        email: 'test@example.com',
+        password: 'password',
+        passwordConfirmation: 'password',
+        username: 'testuser',
+        cellphone: '1234567890',
+      };
+
+      const result = await service.register(registerDto);
+
+      expect(bcrypt.hash).toHaveBeenCalledWith('password', 10);
+      expect(result).toEqual({
+        message: 'User registered successfully',
+      });
+    });
   });
 
   describe('login', () => {
-    it('should throw UnauthorizedException for invalid credentials', async () => {
-      (mockDrizzle.select as jest.Mock).mockReturnValueOnce({
-        from: jest.fn().mockReturnValue({
-          where: jest.fn().mockReturnValue({
-            limit: jest.fn().mockResolvedValue([]),
+    it.each([
+      {
+        scenario: 'invalid email',
+        loginTestDto: {
+          email: 'invalid@example.com',
+          password: 'password',
+        },
+        mockUser: [],
+      },
+      {
+        scenario: 'invalid password',
+        loginTestDto: {
+          email: 'test@example.com',
+          password: 'invalid',
+        },
+        mockUser: [
+          {
+            id: 1,
+            email: 'test@example.com',
+            password: 'hashedPassword',
+          },
+        ],
+      },
+    ])(
+      'should throw UnauthorizedException for $scenario',
+      async ({ loginTestDto, mockUser }) => {
+        (mockDrizzle.select as jest.Mock).mockReturnValueOnce({
+          from: jest.fn().mockReturnValue({
+            where: jest.fn().mockReturnValue({
+              limit: jest.fn().mockResolvedValue(mockUser),
+            }),
           }),
-        }),
-      });
+        });
 
-      const loginDto = {
-        email: 'test@example.com',
-        password: 'password',
-      };
-
-      await expect(service.login(loginDto)).rejects.toThrow(
-        UnauthorizedException,
-      );
-    });
+        await expect(service.login(loginTestDto)).rejects.toThrow(
+          UnauthorizedException,
+        );
+      },
+    );
 
     it('should return access token for valid credentials', async () => {
       (mockDrizzle.select as jest.Mock).mockReturnValueOnce({
@@ -126,7 +233,7 @@ describe('AuthService', () => {
 
       const loginDto = {
         email: 'test@example.com',
-        password: 'password',
+        password: 'Password',
       };
 
       const result = await service.login(loginDto);
