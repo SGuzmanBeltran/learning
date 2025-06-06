5cc4f11d29ca572533c4cca56ad86b1755507648
SGuzmanBeltran <santiagoguzmanbeltran@gmail.com>
2025-06-05 20:14:17 -0500

    create teams

--- js/krown/src/app.module.ts
+++ js/krown/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { DrizzleModule } from './drizzle/drizzle.module';
+import { TeamsModule } from './teams/teams.module';
 
 @Module({
-  imports: [AuthModule, DrizzleModule],
+  imports: [AuthModule, DrizzleModule, TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
